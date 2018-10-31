9bf592e04acf8b5f015be10cedc08940e7e50f54
Eugene Belinski <ebelinski@protonmail.com>
2018-10-31 14:05:17 -0500

    display Hello World in a couple of styles

--- TypeStyle/ViewController.swift
+++ TypeStyle/ViewController.swift
@@ -1,5 +1,50 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+  let base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  let styles = [
+    "ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ",
+    "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ",
+    "₳฿₵ĐɆ₣₲ⱧłJ₭Ⱡ₥₦Ø₱QⱤ₴₮ɄV₩ӾɎⱫ₳฿₵ĐɆ₣₲ⱧłJ₭Ⱡ₥₦Ø₱QⱤ₴₮ɄV₩ӾɎⱫ",
+    "𝔄𝔅ℭ𝔇𝔈𝔉𝔊ℌℑ𝔍𝔎𝔏𝔐𝔑𝔒𝔓𝔔ℜ𝔖𝔗𝔘𝔙𝔚𝔛𝔜ℨ𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔫𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷"
+  ]
+
+  let input = "Hello World"
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+
+  }
+
+  func styledText(forText text: String, rowIndex: Int) -> String {
+    let newStyle = styles[rowIndex]
+
+    let newText: String = String(text.map {
+      if let index = base.firstIndex(of: $0) {
+        return newStyle[index]
+      } else {
+        return $0
+      }
+    })
+
+    return newText
+  }
+
+}
+
+// MARK: UITableView Data Source
+extension ViewController {
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return styles.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel?.text = styledText(forText: input, rowIndex: indexPath.row)
+    return cell
+  }
 
 }
