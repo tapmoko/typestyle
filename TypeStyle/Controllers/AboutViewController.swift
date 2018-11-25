45a43a7f2aa0e16a441401511e310948a32faee7
Eugene Belinski <ebelinski@protonmail.com>
2018-11-24 21:08:34 -0600

    fixed typo

--- TypeStyle/Controllers/AboutViewController.swift
+++ TypeStyle/Controllers/AboutViewController.swift
@@ -9,7 +9,7 @@ class AboutViewController: UITableViewController {
     AboutLabelTableViewCell(text: "TypeStyle is open source! It is written in Swift 4.2, and released under the GNU-GPL 3.0 license."),
     AboutButtonTableViewCell(text: "View Source", link: "https://github.com/ebelinski/typestyle-ios"),
     AboutLabelTableViewCell(text: "The TypeStyle privacy policy is available here:"),
-    AboutButtonTableViewCell(text: "View Source", link: "https://typestyle.app/privacy-policy")
+    AboutButtonTableViewCell(text: "Privacy Policy", link: "https://typestyle.app/privacy-policy")
   ]
 
   override func viewDidLoad() {
