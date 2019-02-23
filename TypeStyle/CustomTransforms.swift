f8d8a86037a81144296acb8d608a091d51d14846
Eugene Belinski <ebelinski@protonmail.com>
2019-02-23 13:06:39 -0600

    created blank mockingSpongeBob transform

---
+++ TypeStyle/CustomTransforms.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct CustomTransforms {
+  
+  static let mockingSpongeBob: (String) -> String = { input in
+    // TODO: Implement this transform
+    return input
+  }
+  
+}
