e98111c7a259d433dd4373fc3dccbb2ab73c410d
Eugene Belinski <ebelinski@protonmail.com>
2019-11-02 22:08:10 -0500

    added tests target

---
+++ TypeStyleTests/TypeStyleTests.swift
@@ -0,0 +1,33 @@
+//
+//  TypeStyleTests.swift
+//  TypeStyleTests
+//
+//  Created by PSD on 11/2/19.
+//  Copyright © 2019 Eugeniu Belinski. All rights reserved.
+//
+
+import XCTest
+
+class TypeStyleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
