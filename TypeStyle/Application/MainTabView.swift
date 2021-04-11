82ca8f1e7334b851f3167fc25f654887a1193766
Eugene Belinski <ebelinski@protonmail.com>
2021-04-10 20:13:54 -0500

    Created MainTabView.

---
+++ TypeStyle/Application/MainTabView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct MainTabView: View {
+  var body: some View {
+    TabView {
+      Text("Styles page")
+        .tabItem {
+          Label("Styles", systemImage: "bold.italic.underline")
+        }
+
+      Text("Decorations page")
+        .tabItem {
+          Label("Decorations", systemImage: "wand.and.stars")
+        }
+
+      Text("Emoticons page")
+        .tabItem {
+          Label("Emoticons", systemImage: "smiley")
+        }
+
+      Text("About page")
+        .tabItem {
+          Label("About", systemImage: "info.circle")
+        }
+    }
+  }
+}
--- TypeStyle/Application/TypeStyleApp.swift
+++ TypeStyle/Application/TypeStyleApp.swift
@@ -4,27 +4,7 @@ import SwiftUI
 struct TypeStyleApp: App {
   var body: some Scene {
     WindowGroup {
-      TabView {
-        Text("Styles page")
-          .tabItem {
-            Label("Styles", systemImage: "bold.italic.underline")
-          }
-
-        Text("Decorations page")
-          .tabItem {
-            Label("Decorations", systemImage: "wand.and.stars")
-          }
-
-        Text("Emoticons page")
-          .tabItem {
-            Label("Emoticons", systemImage: "smiley")
-          }
-
-        Text("About page")
-          .tabItem {
-            Label("About", systemImage: "info.circle")
-          }
-      }
+      MainTabView()
     }
   }
 }
