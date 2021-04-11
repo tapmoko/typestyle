14e7750468545cbb395b2e265ceef4adab59fe50
Eugene Belinski <ebelinski@protonmail.com>
2021-04-10 20:29:17 -0500

    Request StoreKit products.

--- TypeStyle/Application/MainTabView.swift
+++ TypeStyle/Application/MainTabView.swift
@@ -25,3 +25,9 @@ struct MainTabView: View {
     }
   }
 }
+
+struct MainTabView_Previews: PreviewProvider {
+  static var previews: some View {
+    MainTabView()
+  }
+}
--- TypeStyle/Application/TypeStyleApp.swift
+++ TypeStyle/Application/TypeStyleApp.swift
@@ -2,9 +2,46 @@ import SwiftUI
 
 @main
 struct TypeStyleApp: App {
+
+  @Environment(\.scenePhase) private var scenePhase
+
+  @State var isInitialDidBecomeActive = true
+
   var body: some Scene {
     WindowGroup {
       MainTabView()
+        .onChange(of: scenePhase) { handle(scenePhase: $0) }
     }
   }
+
+  private func handle(scenePhase: ScenePhase) {
+    switch scenePhase {
+    case .active: didBecomeActive()
+    case .inactive: didBecomeInactive()
+    case .background: didEnterBackground()
+    @unknown default: break
+    }
+  }
+
+  // The scene is in the foreground and interactive.
+  private func didBecomeActive() {
+    if !isInitialDidBecomeActive { return }
+
+    Products.store.requestProducts { success, tipProducts in
+      if success {
+        Products.tipProducts = tipProducts!
+      }
+    }
+
+    isInitialDidBecomeActive = false
+  }
+
+  // The scene is in the foreground but should pause its work.
+  private func didBecomeInactive() {
+  }
+
+  // The scene isn’t currently visible in the UI.
+  private func didEnterBackground() {
+  }
+
 }
