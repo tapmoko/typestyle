b7a1a09ce4868fdfdf25f874b1a071d91478a876
Eugene Belinski <ebelinski@protonmail.com>
2021-04-10 19:25:54 -0500

    Imported existing colors.

---
+++ TypeStyle/Application/Extensions/Color+.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+extension Color {
+
+  static let appBackground = Color(#colorLiteral(red: 0.514, green: 0.294, blue: 0.639, alpha: 1)) // #834ba3
+  static let appDarkBackground = Color(#colorLiteral(red: 0.247, green: 0.169, blue: 0.306, alpha: 1)) // #3f2b4e
+  static let appSelected = Color(#colorLiteral(red: 0.659, green: 0.435, blue: 0.784, alpha: 1)) // #a86fc8
+  static let appText = Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)) // #fff
+  static let appFadedText = Color(#colorLiteral(red: 0.667, green: 0.667, blue: 0.667, alpha: 1)) // #aaa
+
+}
